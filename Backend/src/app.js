b265816167864586e14a59a71e6afd2e5788443d
Yash Singh <yashsingh.work.01@gmail.com>
2025-05-15 08:05:40 +0530

    enhance security

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,24 +1,30 @@
 const express = require('express');
+require('dotenv').config();
 require('./config/envValidator');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const csrf = require('csurf');
 const helmet = require('helmet');
 const Limiter = require('./middlewares/rateLimit.middleware');
 const dbConnection = require('./db/connection');
 const authRoute = require('./routes/auth.route');
-require('dotenv').config();
-const app = express();
 
+
+const app = express();
 dbConnection();
+
+app.set('trust proxy', 1);
+app.use(helmet());
+app.use(Limiter.global);
 app.use(cors({
     origin: [process.env.ORIGIN, 'http://localhost:5173'],
     credentials: true
 }));
-app.use(helmet());
-app.use(Limiter.global);
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(csrfProtection);
+const csrfProtection = csrf({ cookie: true });
 
 app.use('/api/auth', authRoute);
 
