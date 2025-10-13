412cf25d533d4d057ce36b50365a0c60244f73d8
Yash Singh <yashsingh.work.01@example.com>
2025-10-13 16:03:48 +0530

    update db connection

--- Backend/src/db/connection.js
+++ Backend/src/db/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 const logger = require('../config/logger.config');
 
-function dbConnection() {
-    mongoose.connect(process.env.DB_URL)
-        .then(logger.info('Database Connected'))
-        .catch((error) => logger.error('Error in database connection: ', error));
+async function dbConnection() {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+        logger.info('Database Connected')
+    } catch (error) {
+        logger.error('Error in database connection: ', error.message);
+        process.exit(1);
+    }
 }
 
 module.exports = dbConnection;
