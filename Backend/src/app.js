9b44fe0f77f6b8540aff242e9cb332edcfd3cb3b
Yash Singh <yashsingh.work.01@example.com>
2025-10-17 09:11:40 +0530

    remove dotenv-safe package

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 require('dotenv').config();
-require('./config/envValidator');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const csrf = require('csurf');
