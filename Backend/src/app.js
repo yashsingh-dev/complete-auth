2d2e1985eb169104c3482d483143640f092ae632
Yash Singh <yashsingh.work.01@example.com>
2025-10-17 09:29:28 +0530

    update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,28 +4,28 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const csrf = require('csurf');
 const helmet = require('helmet');
+
 const Limiter = require('./middlewares/rateLimit.middleware');
 const dbConnection = require('./db/connection');
 const authRoute = require('./routes/auth.route');
 
-
 const app = express();
-dbConnection();
 const csrfProtection = csrf({ cookie: true });
 
+dbConnection();
 app.set('trust proxy', 1);
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false, }));
 app.use(Limiter.global);
 app.use(cors({
-    CLIENT_URL_DEV: [process.env.CLIENT_URL_DEV, 'http://localhost:5173'],
+    origin: [process.env.CLIENT_URL_DEV, 'http://localhost:5173'],
     credentials: true
 }));
 app.use(cookieParser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(csrfProtection);
 
 app.use('/api/auth', authRoute);
-app.use(csrfProtection);
 
 
 module.exports = app;
