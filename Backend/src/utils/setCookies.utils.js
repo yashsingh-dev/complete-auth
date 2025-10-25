125a2aeb0e44e03a3b16d4d54d5a49f900215226
Yash Singh <yashsingh.work.01@gmail.com>
2025-10-25 08:50:26 +0530

    Refactor cookie handling to use common options for setting and clearing tokens

--- Backend/src/utils/setCookies.utils.js
+++ Backend/src/utils/setCookies.utils.js
@@ -1,35 +1,26 @@
 const { COOKIES } = require("../config/constants");
 
+const commonCookieOptions = {
+    httpOnly: true,
+    secure: (process.env.NODE_ENV || 'development') === 'production',
+    sameSite: 'Lax',
+    path: '/',
+    domain: process.env.COOKIE_DOMAIN || undefined
+};
+
 module.exports.setAuthTokens = async function (res, cookieName, token, maxAge) {
 
     res.cookie(cookieName, token, {
-        httpOnly: true,
-        secure: (process.env.NODE_ENV || 'development') === 'production',
-        sameSite: 'Strict',
-        maxAge
+        ...commonCookieOptions,
+        maxAge: maxAge
     });
 };
 
 module.exports.clearToken = async function (res, cookieName) {
-
-    res.clearCookie(cookieName, {
-        httpOnly: true,
-        secure: (process.env.NODE_ENV || 'development') === 'production',
-        sameSite: 'Strict'
-    });
+    res.clearCookie(cookieName, commonCookieOptions);
 };
 
 module.exports.clearTokenCookies = async function (res) {
-
-    res.clearCookie(COOKIES.ACCESS_TOKEN, {
-        httpOnly: true,
-        secure: (process.env.NODE_ENV || 'development') === 'production',
-        sameSite: 'Strict'
-    });
-
-    res.clearCookie(COOKIES.REFRESH_TOKEN, {
-        httpOnly: true,
-        secure: (process.env.NODE_ENV || 'development') === 'production',
-        sameSite: 'Strict'
-    });
+    res.clearCookie(COOKIES.ACCESS_TOKEN, commonCookieOptions);
+    res.clearCookie(COOKIES.REFRESH_TOKEN, commonCookieOptions);
 };
