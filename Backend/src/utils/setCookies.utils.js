49ac8a29eb49195ac24fb7a9dff7ab11eb0a7d7d
Yash Singh <yashsingh.work.01@gmail.com>
2025-10-25 13:20:05 +0530

    Fix secure cookie option to use environment variable for production

--- Backend/src/utils/setCookies.utils.js
+++ Backend/src/utils/setCookies.utils.js
@@ -3,8 +3,7 @@ const logger = require('../config/logger.config');
 
 const commonCookieOptions = {
     httpOnly: true,
-    // secure: (process.env.NODE_ENV || 'development') === 'production',
-    secure: 'true',
+    secure: (process.env.NODE_ENV || 'development') === 'production',
     sameSite: 'None',
     path: '/',
     domain: process.env.COOKIE_DOMAIN || undefined
