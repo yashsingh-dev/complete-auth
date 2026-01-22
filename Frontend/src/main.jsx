4d8653113fb6efde845393afb25210699d833199
Yash Singh <yashsingh.work.01@gmail.com>
2026-01-22 22:08:56 +0530

    skip-1

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,8 +3,9 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
